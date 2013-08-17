56cd5eb2d83631649cffaf89dfe525c6ae2e4eef
cvanderschuere <cdvconcepts@gmail.com>
2013-08-16 20:59:32 -0700

    Working search with metadata api

---
+++ iOSClient/src/SpotifyResult.m
@@ -0,0 +1,13 @@
+//
+//  SpotifyResult.m
+//  MusicBox-iOS
+//
+//  Created by Chris Vanderschuere on 8/15/13.
+//  Copyright (c) 2013 CDVConcepts. All rights reserved.
+//
+
+#import "SpotifyResult.h"
+
+@implementation SpotifyResult
+
+@end
